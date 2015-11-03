ba3a6931c7b9454920be31a0466afefd784f978f
Harald Kirschner <hkirschner@mozilla.com>
2015-11-03 12:03:02 -0800

    Force HTTPS via JS, closes #15

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,11 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('offline-worker.js').then(function(registration) {
-    console.log('offline worker registered');
-  });
-}
+(function() {
+  if (location.hostname !== 'localhost' && location.protocol !== 'https:') {
+    location.href = location.href.replace(/^http/, 'https');
+    return;
+  }
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('offline-worker.js').then(function(registration) {
+      console.log('offline worker registered');;
+    });
+  }
+})();
