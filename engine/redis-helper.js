93b49982e5d03742f5f99ba5af274d2b95497fd5
Piotr Zalewa <piotr@zalewa.info>
2016-02-01 21:43:48 +0100

    redis variable changed

--- engine/redis-helper.js
+++ engine/redis-helper.js
@@ -1,7 +1,7 @@
 import redis from 'redis';
 
 function getClient(dbTestNumber) {
-  const client = redis.createClient(process.env.REDISCLOUD_URL, { no_ready_check: true });
+  const client = redis.createClient(process.env.REDIS_URL, { no_ready_check: true });
   return new Promise((resolve, reject) => {
     if (!dbTestNumber) {
       return resolve(client);
