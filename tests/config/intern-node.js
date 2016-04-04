3e60198208e612d4e7794674642f306777b436d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-04-04 15:48:11 +0100

    Always define functionalSuites as an empty array, to work around https://github.com/theintern/intern/issues/600

--- tests/config/intern-node.js
+++ tests/config/intern-node.js
@@ -5,6 +5,7 @@ define({
   },
 
   suites: ['tests/nodeUnit', 'tests/redisUnit', 'tests/apiUnit', 'tests/cacheUnit'],
+  functionalSuites: [],
 
   // A regexp to exclude from code coverage calculations
   excludeInstrumentation: /^(?:tests|node_modules)\//,
